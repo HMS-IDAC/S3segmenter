d194b378170154e4062332bfa34048e96bef4a51
Yu-An Chen <atwood12@gmail.com>
2022-05-12 02:50:18 -0400

    Delete matlab files

--- S3punctaDetection.m
+++
@@ -1,6 +0,0 @@
-function spots = S3punctaDetection(image,sigma,alpha)
-    Ilog = filterLoG(image,sigma);
-    Imax = imregionalmax(Ilog);
-    threshold = median(Ilog(Imax))+ alpha*1.4826*mad(Ilog(Imax),1);
-    spots = Imax.*(Ilog>threshold);
-end
--- normI.m
+++
@@ -1,8 +0,0 @@
-function J = normI(I)
-    Irs=imresize(I,0.1,'nearest');
-    p1 = prctile(Irs(:),0.1);
-    J = I-p1;
-%     Jrs= imresize(J,0.05,'nearest');
-    p99 = prctile(Irs(:),99.99);
-    J = J/(p99-p1);
-end
