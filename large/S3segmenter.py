5e94d7cadebc1c8204931a4e999c9b4db435a75a
Yu-An Chen <atwood12@gmail.com>
2022-09-01 02:24:22 -0400

    Parse pixel size from ome-xml and ask user to provide if failed

--- large/S3segmenter.py
+++ large/S3segmenter.py
@@ -3,6 +3,8 @@ import sys
 import pathlib 
  
 import watershed 
+import s3seg_util
+import logging
  
 def main(argv=sys.argv): 
  
@@ -27,6 +29,11 @@ def main(argv=sys.argv):
         '--outputPath', 
         default='.' 
     ) 
+    parser.add_argument( 
+        '--pixelSize', 
+        default=None,
+        type=float 
+    )
  
     args, extra_argv = parser.parse_known_args(argv[1:]) 
  
@@ -37,10 +44,22 @@ def main(argv=sys.argv):
     out_path.parent.mkdir(exist_ok=True, parents=True) 
  
     return watershed.main([ 
+    if args.pixelSize is not None:
+        pixel_size = args.pixelSize
+    else:
+        pixel_size = s3seg_util.detect_pixel_size(img_path)
+        if pixel_size is None:
+            logging.error(
+                'Auto-detect pixel size failed, use `--pixelSize SIZE` to specify it'
+            )
+            return 1
+
+    watershed.main([ 
         '', 
         '-i', args.stackProbPath, 
         '-o', str(out_path), 
         '--mcmicro', 
+        '--pixel-size', str(pixel_size),
         *extra_argv 
     ]) 
 
